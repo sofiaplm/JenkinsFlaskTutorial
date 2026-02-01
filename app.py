22a4aa6f35fcefa8025c6a9ab6722159b9454158
SOFIA <s.ploumistou@gmail.com>
2026-02-01 19:36:02 +0200

    Update app message and add gitignore

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return jsonify({"message": "Welcome to the Flask API!"})
+    return jsonify({"message": "Welcome to the Flask API!, Jenkins is working perfectly via Docker!"})
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
